52a04eefa6d551a44d7254d1699ffa31b0443e2e
DESKTOP-378FDDD\Markus <kosyk.markian@gmail.com>
2023-05-18 18:26:55 +0300

    register validation validation done

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,9 +1,84 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Register.module.css';
 import { NavLink } from 'react-router-dom';
+import { Axios } from 'axios';
+import { useRef } from 'react';
+
+
+
 
 
 function Register() {
+    //creating variables for inputsFields
+    const email = useRef(null)
+    const password = useRef(null);
+    const repeatPassword = useRef(null);
+
+    const [emailError, setEmailError] = useState(false);
+    const [passwordError, setPasswordEror] = useState(false);
+    const [repeatPasswordError, setRepeatPasswordError] = useState(false);
+    const [errorText, setErrorText] = useState('');
+
+   
+
+    const checkUser = (email, password, repeatPassword) => {
+        setEmailError(false);
+        setPasswordEror(false);
+        setRepeatPasswordError(false);
+        setErrorText('');
+
+
+        const regexp = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+        if(!email.match(regexp))
+        {
+            setEmailError(true)
+            setErrorText('Неправильний email');
+            return 1;
+        }
+
+        if(password.length < 8)
+        {
+            setPasswordEror(true);
+            setErrorText('Пароль повинен містити більше ніж 8 символів');
+            return 1;
+        }
+
+        if(password != repeatPassword)
+        {
+            setRepeatPasswordError(true);
+            setErrorText('Пароль і повторний пароль повинні збігатися');
+            return 1;
+        }
+    }
+
+
+
+   
+
+    const Submit = (e) => {
+
+        e.preventDefault(); 
+
+        let emailSubmit = email.current.value;
+        let passwordSubmit = password.current.value;
+        let repeatPasswordSubmit = repeatPassword.current.value;
+
+        const resultCheckUser = checkUser(emailSubmit, passwordSubmit, repeatPasswordSubmit);
+
+        if (resultCheckUser === 1) {
+            return;
+        }
+
+        
+
+
+        console.log(emailSubmit, passwordSubmit, repeatPasswordSubmit);
+    }
+
+
+    
+
     return (
         <div className={styles.registerBlock}>
             <div className={styles.Register}>
@@ -15,15 +90,18 @@ function Register() {
                     <div className={styles.registerForm}>
                         <div className={styles.inputsBlock}>
                             <span className={styles.nameForFields}>Email Address</span>
-                            <input type='email' className={styles.inputsForFields} required />
+                            <input type='email' className={styles.inputsForFields} required ref={email} />
+                            <p className={emailError ? styles.errorParagrpah : styles.errorParagrpahInvisible }>{errorText}</p>
                         </div>
                         <div className={styles.inputsBlock}>
                             <span className={styles.nameForFields}>Password</span>
-                            <input type='password' className={styles.inputsForFields} required />
+                            <input type='password' className={styles.inputsForFields} required ref={password} />
+                            <p className={passwordError ? styles.errorParagrpah : styles.errorParagrpahInvisible }>{errorText}</p>
                         </div>
                         <div className={styles.inputsBlock}>
                             <span className={styles.nameForFields}>Repeat Password</span>
-                            <input type='password' className={styles.inputsForFields} required />
+                            <input type='password' className={styles.inputsForFields} required ref={repeatPassword} />
+                            <p className={repeatPasswordError ? styles.errorParagrpah : styles.errorParagrpahInvisible }>{errorText}</p>
                         </div>
                         <div className={styles.inputsBlockCheckcbox}>
                             <div className={styles.CheckboxWrapper}>
@@ -33,7 +111,7 @@ function Register() {
                         </div>
                         <div className={styles.Submit}>
                             <div className={styles.buttonWrapper}>
-                                <button>Countinue</button>
+                                <button onClick={(e) => { Submit(e) }}>Countinue</button>
                             </div>
                         </div>
                         <div className={styles.accountExists}>
