f33e5b3a55b195b0c2b186930521dac3e588bfa8
DESKTOP-378FDDD\Markus <kosyk.markian@gmail.com>
2023-05-19 14:11:48 +0300

    new variant of diagramm

--- src/components/UserPage/index.js
+++ src/components/UserPage/index.js
@@ -1,25 +1,25 @@
-
-
-import React from "react";
+import React, { useEffect } from "react";
 import ApexCharts from 'apexcharts';
-import { useEffect } from "react";
 
 import styles from './style.module.css';
 import Logo from "../MainPage/Logo";
 
 function UserPage() {
-
   useEffect(() => {
     var options = {
       chart: {
         type: 'donut'
       },
-      series: [44, 55, 41,56],
-      chartOptions: {
-        labels: {
-          show: true
-        }
-
+      series: [44, 55, 41, 56],
+      labels: ['Apple', 'Banana', 'Cherry', 'Durian'],
+      annotations: {
+        position: 'center',
+        text: 'total: 145',
+        textAnchor: 'middle',
+        fontSize: '20px',
+        fontFamily: 'Helvetica, Arial, sans-serif',
+        fontWeight: 'bold',
+        textColor: '#000000'
       },
       plotOptions: {
         pie: {
@@ -37,16 +37,11 @@ function UserPage() {
     };
   }, []);
 
-
-
   return (
     <div className={styles.userpage}>
       <div className={styles.userpage_container}>
-
         <div className={styles.content_header}>
-
           <div className={styles.content_header_logo}><Logo /></div>
-
           <div className={styles.content_header_navbar}>
             <div className={styles.navbar_item}>About Us</div>
             <div className={styles.navbar_item}>Our Project</div>
@@ -58,14 +53,9 @@ function UserPage() {
         <div className={styles.Diagramm}>
           <div id="chart"></div>
         </div>
-
-
       </div>
-
-
     </div>
   )
 }
-
 
 export default UserPage;
