5805a7dd6c913128912866af261d480c9ee6ed47
DESKTOP-378FDDD\Markus <kosyk.markian@gmail.com>
2023-05-19 13:59:27 +0300

    diagram done

--- src/components/UserPage/index.js
+++ src/components/UserPage/index.js
@@ -1,32 +1,70 @@
 
 
 import React from "react";
+import ApexCharts from 'apexcharts';
+import { useEffect } from "react";
 
 import styles from './style.module.css';
 import Logo from "../MainPage/Logo";
 
 function UserPage() {
-    return(
-        <div className={styles.userpage}>
-            <div className={styles.userpage_container}>
 
-            <div className={styles.content_header}>
+  useEffect(() => {
+    var options = {
+      chart: {
+        type: 'donut'
+      },
+      series: [44, 55, 41,56],
+      chartOptions: {
+        labels: {
+          show: true
+        }
 
-                <div className={styles.content_header_logo}><Logo/></div>
-                
-                <div className={styles.content_header_navbar}>
-                  <div className={styles.navbar_item}>About Us</div>
-                  <div className={styles.navbar_item}>Our Project</div>
-                  <div className={styles.navbar_item}>Money+</div>
-                  <div className={styles.navbar_item}>Info</div>
-                  <div className={styles.navbar_item}>QA</div>
-                </div>
+      },
+      plotOptions: {
+        pie: {
+          customScale: 1
+        }
+      }
+    }
 
-              </div>
+    var chart = new ApexCharts(document.querySelector("#chart"), options);
+    chart.render();
 
-            </div>
+    // Зупинити або видалити графік при знищенні компоненти
+    return () => {
+      chart.destroy();
+    };
+  }, []);
+
+
+
+  return (
+    <div className={styles.userpage}>
+      <div className={styles.userpage_container}>
+
+        <div className={styles.content_header}>
+
+          <div className={styles.content_header_logo}><Logo /></div>
+
+          <div className={styles.content_header_navbar}>
+            <div className={styles.navbar_item}>About Us</div>
+            <div className={styles.navbar_item}>Our Project</div>
+            <div className={styles.navbar_item}>Money+</div>
+            <div className={styles.navbar_item}>Info</div>
+            <div className={styles.navbar_item}>QA</div>
+          </div>
         </div>
-    )
+        <div className={styles.Diagramm}>
+          <div id="chart"></div>
+        </div>
+
+
+      </div>
+
+
+    </div>
+  )
 }
 
 
